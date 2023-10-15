2e90e34d80834066b90cccddcd34f4b2160872cb
amirafauzey <amirafauzey94@gmail.com>
2023-10-15 15:17:56 +0800

    fix testing code

--- webServices/src/test/java/com/mira/webServices/service/PostcodeLatitudeLongitudeServiceTest.java
+++ webServices/src/test/java/com/mira/webServices/service/PostcodeLatitudeLongitudeServiceTest.java
@@ -14,9 +14,13 @@ class PostcodeLatitudeLongitudeServiceTest {
 
     @Test
     void calculateDistance() {
+
+
+        //We use repository because simulation with database
         // Create a mock repository
         PostcodeLatitudeLongitudeRepository repository = Mockito.mock(PostcodeLatitudeLongitudeRepository.class);
 
+        //Because we want to simulate  and check that function does what is supposed to do
         // Create a test instance of the service with the mock repository
         PostcodeLatitudeLongitudeService service = new PostcodeLatitudeLongitudeService(repository);
 
@@ -32,6 +36,7 @@ class PostcodeLatitudeLongitudeServiceTest {
         location2.setLongitude(-2.1214880);
 
         // Mock the behavior of the repository to return the sample data when findByPostcode is called
+        // We simulate behaviour of database
         Mockito.when(repository.findByPostcode("AB10 1XG")).thenReturn(location1);
         Mockito.when(repository.findByPostcode("AB10 6RM")).thenReturn(location2);
 
@@ -39,11 +44,12 @@ class PostcodeLatitudeLongitudeServiceTest {
         DistanceResponse response = service.calculateDistance("AB10 1XG", "AB10 6RM");
 
         // Perform assertions on the response
+        //
         assertNotNull(response);
-        assertEquals("AB10 1XG", response.getLocation1().getPostalCode());
+        assertEquals("AB10 1XG", response.getLocation1().getPostcode());
         assertEquals(57.1441560, response.getLocation1().getLatitude());
         assertEquals(-2.1148640, response.getLocation1().getLongitude());
-        assertEquals("AB10 6RM", response.getLocation2().getPostalCode());
+        assertEquals("AB10 6RM", response.getLocation2().getPostcode());
         assertEquals(57.1378720, response.getLocation2().getLatitude());
         assertEquals(-2.1214880, response.getLocation2().getLongitude());
         assertEquals(0.8, response.getDistance(), 0.01); // Use delta for double comparisons
